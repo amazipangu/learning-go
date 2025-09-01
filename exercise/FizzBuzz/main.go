6fe35db25a8c53a8ee806b5f6d8299c45fcd76b6
amazipangu <s.pulscord@gmail.com>
2025-09-01 09:46:53 +0900

    Append the item depending on divisibility rules (FizzBuzz)

--- exercise/FizzBuzz/main.go
+++ exercise/FizzBuzz/main.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func fizzBuzz(n int) []string {
 	var answer []string
-	if n%3 == 0 && n%5 == 0 {
-		fmt.Println("FizzBuzz")
-	} else if n%3 == 0 {
-		fmt.Println("Fizz")
-	} else if n%5 == 0 {
-		fmt.Println("Buzz")
-	} else {
-		fmt.Println("oops, let's choose another number!")
+
+	for i := 0; i < n; i++ {
+		j := i + 1
+
+		if j%3 == 0 && j%5 == 0 {
+			answer = append(answer, "FizzBuzz")
+		} else if j%3 == 0 {
+			answer = append(answer, "Fizz")
+		} else if j%5 == 0 {
+			answer = append(answer, "Buzz")
+		} else {
+			answer = append(answer, strconv.Itoa(j))
+		}
+
 	}
 
+	fmt.Println(answer) /* [1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz] */
 	return answer
 }
 
 func main() {
-	fizzBuzz(10)
+	fizzBuzz(15)
 }
