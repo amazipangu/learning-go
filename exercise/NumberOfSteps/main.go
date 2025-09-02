68567e4dd7a358c886bc41b8d2bbe22bced09a11
amazipangu <s.pulscord@gmail.com>
2025-09-02 09:08:53 +0900

    Use a condition-only `for` loop like a while statement (C, Python, or Java)

--- exercise/NumberOfSteps/main.go
+++ exercise/NumberOfSteps/main.go
@@ -1,8 +1,17 @@
 package main
 
 func numberOfSteps(num int) int {
-	var result int = 0
-	return result
+	var steps int = 0
+
+	for num > 0 {
+		if num%2 == 0 {
+			num /= 2
+		} else {
+			num--
+		}
+		steps++
+	}
+	return steps
 }
 
 func main() {
