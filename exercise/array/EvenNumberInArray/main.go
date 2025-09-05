72716e69a196d7d59846c4d342188a897c328465
amazipangu <s.pulscord@gmail.com>
2025-09-05 11:18:06 +0900

    Added the initial solution

--- exercise/array/EvenNumberInArray/main.go
+++ exercise/array/EvenNumberInArray/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func findNumbers(nums []int) int {
+	var numberString string
+	var evenNumberDigitsCount int = 0
+	var tempArray []int
 
+	for i := 0; i < len(nums); i++ {
+		numberString = strconv.Itoa(nums[i])
+		if len(numberString)%2 == 0 {
+			tempArray = append(tempArray, len(numberString))
+		}
+		if len(tempArray) > evenNumberDigitsCount {
+			evenNumberDigitsCount = len(tempArray)
+		}
+	}
+
+	return evenNumberDigitsCount
 }
 
 func main() {
