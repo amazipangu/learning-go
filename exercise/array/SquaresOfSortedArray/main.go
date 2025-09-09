f8f1524b9c8d5bcfdd462085bb7194b500e8c2c3
amazipangu <s.pulscord@gmail.com>
2025-09-09 17:45:22 +0900

    Added the initial solution

--- exercise/array/SquaresOfSortedArray/main.go
+++ exercise/array/SquaresOfSortedArray/main.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func sortedSquares(nums []int) []int {
 	var sortedArray []int
+
+	for _, num := range nums {
+		sortedArray = append(sortedArray, num*num)
+	}
+
 	return sortedArray
 }
 
 func main() {
-	fmt.Println(sortedSquares([]int{-4, -1, 0, 3, 10}))
+	nums := []int{-4, -1, 0, 3, 10}
+	fmt.Println(sortedSquares(nums))
 }
