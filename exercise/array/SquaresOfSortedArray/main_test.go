ab6ecf1741756e9bfa677a2ee71fbe5622172d69
amazipangu <s.pulscord@gmail.com>
2025-09-10 19:03:08 +0900

    Added the test case

--- exercise/array/SquaresOfSortedArray/main_test.go
+++ exercise/array/SquaresOfSortedArray/main_test.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestFindNumbers(t *testing.T) {
+func TestSortedSquares(t *testing.T) {
 	t.Run("Return an array of the squares of each number", func(t *testing.T) {
-
+		nums := []int{12, 345, 2, 6, 7896}
+		expect := []int{4, 36, 144, 119025, 62346816}
+		assert.Equal(t, expect, sortedSquares(nums))
 	})
 }
