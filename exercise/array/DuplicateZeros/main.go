0716a8904ab544041ca077c6be46c4ea837ff7a6
amazipangu <s.pulscord@gmail.com>
2025-09-12 19:05:06 +0900

    Added skelton code

---
+++ exercise/array/DuplicateZeros/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func duplicatedZeros(arr []int) []int {
+	var dupZerosArray []int
+	return dupZerosArray
+}
+
+var arr = []int{1, 0, 2, 3, 0, 4, 5, 0}
+
+func main() {
+	fmt.Println(duplicatedZeros(arr))
+}
