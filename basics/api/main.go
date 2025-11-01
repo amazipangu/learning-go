21cc49c99e3df52910172371ace42d68318a4334
Shinya <1659749+amazipangu@users.noreply.github.com>
2025-11-01 19:39:51 +0900

    Added /albums endpoint

--- basics/api/main.go
+++ basics/api/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"net/http"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type Album struct {
@@ -12,12 +12,18 @@ type Album struct {
 	Price  float64 `json:"price"`
 }
 
-var albums []Album(
-{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-)
+var albums = []Album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
 
 func GetAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", GetAlbums)
+	router.Run("localhost:8080")
 }
